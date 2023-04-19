b4f689a797ccba182a4b5425150d19944a4d3260
Maukan Mir <maukanm22@gmail.com>
2023-04-19 18:44:27 -0400

    Working on adding new features

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,11 +3,10 @@
 
 </script>
 
-
-
 <body>
-    <div class="header">
 
+    <div class="header">
+        <h1>Manufacturing Consent</h1>
     </div>
 
 
@@ -15,6 +14,10 @@
 
 <style global>
     :global(body){
+    }
+
+    .header{
+        color:whitesmoke;
     }
 
 </style>
