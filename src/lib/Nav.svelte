dfcebbb5c955f21a70ad4a90e4ab0e0b1ae97875
Maukan Mir <maukanm22@gmail.com>
2023-04-19 19:05:55 -0400

    Added hovering ability

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -4,18 +4,28 @@ import "../app.css"
 
 <body>
     <div class="container">
-        <h1 class ="header">Manufacturing Consent</h1>
-    </div>
+            <h1 class ="header">Manufacturing Consent</h1>
+            <h2>Sustainability</h2>
+            <h2>Live</h2>
+            <h2>Conflict</h2>
+            <h2>Sign In</h2>
+        </div>
 
 </body>
 
 <style global>
     :global(body){
     }
-
     .container{
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
         border: 2px solid whitesmoke;
+        padding:12px;
     }
-    
+
+    .container:hover{
+        color:blueviolet;
+    }
 
 </style>
