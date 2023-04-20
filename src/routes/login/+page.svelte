b99528a4ee2cc1bd1ee01747ba95868729e4879c
Maukan Mir <maukanm22@gmail.com>
2023-04-19 20:57:48 -0400

    Added new changes

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,14 +1,28 @@
 <script>
 import "../../app.css";
+import NavBar from "../../lib/Nav.svelte"
 import {userSession} from "../../stores/userSession";
 </script>
 
+<NavBar/>
 <body>
 
+    <div class="container">
+
+    </div>
 </body>
 
 <style global>
     :global(body){
 
     }
+
+    .container{
+        display: flex;
+        flex-direction: column;
+        background-color: white;
+        height:400px;
+        width:600px;
+    }
+
 </style>
