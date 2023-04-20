992d8df796fe701599aa411cd18d54c233a11935
Maukan Mir <maukanm22@gmail.com>
2023-04-19 20:15:39 -0400

    Added new styles

---
+++ src/stores/userSession.js
@@ -0,0 +1,31 @@
+import { writable } from 'svelte/store';
+
+function createSessionStorageStore(key, initialValue) {
+  let value = initialValue;
+  
+  if (typeof window !== 'undefined' && window.localStorage) {
+    const storedValue = window.localStorage.getItem(key);
+    if (storedValue !== null) {
+      value = JSON.parse(storedValue);
+    }
+
+    const store = writable(value);
+  
+    store.subscribe(value => {
+      window.localStorage.setItem(key, JSON.stringify(value));
+    });
+
+    return store;
+  }
+
+  const store = writable(value);
+
+  return store;
+}
+
+export const userSession = createSessionStorageStore('userSession', {
+authenticated: false,
+user: null,
+_id: null,
+email: null
+});
