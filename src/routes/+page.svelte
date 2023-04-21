e0874201f093a3d9735cb32d6e60b3c743c7398f
Maukan Mir <maukanm22@gmail.com>
2023-04-21 14:31:48 -0400

    Added new file changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,47 +1,43 @@
+<!-- App.svelte -->
 <script>
-    import NavBar from "../lib/Nav.svelte"
-    import {userSession} from "../stores/userSession"
-    require('dotenv').config();
+    import dotenv from 'dotenv';
+    dotenv.config();
+    const RAPID_NAME_KEY = process.env.RAPID_NAME_KEY;
+    const RAPID_API_KEY = process.env.RAPID_API_KEY;
+    const RAPID_NAME_HOST = process.env.RAPID_NAME_HOST;
+    const RAPID_HOST = process.env.RAPID_HOST;
+    const RAPID_BASE_URL = process.env.RAPID_BASE_URL;
+    console.log(RAPID_NAME_KEY, RAPID_API_KEY, RAPID_NAME_HOST, RAPID_HOST, RAPID_BASE_URL);
 
-    RAPID_NAME_KEY = process.env.RAPID_NAME_KEY
-    RAPID_API_KEY = process.env.RAPID_API_KEY;
-
-    RAPID_NAME_HOST = process.env.RAPID_NAME_HOST
-    RAPID_HOST = process.env.RAPID_HOST
-    RAPID_URL = process.env.RAPID_BASE_URL
+    import NavBar from "../lib/Nav.svelte";
+    import { userSession } from "../stores/userSession";
     import { onMount } from "svelte";
-
-
-    const rapidResultsAPI = ()=>{
-        const options = {
-            method:"GET",
-            headers:{
-                RAPID_NAME_KEY: RAPID_API_KEY,
-		        RAPID_HOST: RAPID_HOST
-            }
-        }
-
-    const rapid_results = fetch(RAPID_BASE_URL, options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
-
-    console.log(rapidResultsAPI)
-    }
-
+  
+    const rapidResultsAPI = () => {
+      const options = {
+        method: "GET",
+        headers: {
+          [RAPID_NAME_KEY]: RAPID_API_KEY,
+          [RAPID_NAME_HOST]: RAPID_HOST,
+        },
+      };
+  
+      fetch(RAPID_BASE_URL, options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+    };
+  
     let user;
     onMount(async () => {
-    userSession.subscribe(storeValue => {
+      userSession.subscribe(storeValue => {
         if (storeValue.user) {
-            user = storeValue.user;
+          user = storeValue.user;
         }
+      });
+      rapidResultsAPI();
     });
-    rapidResultsAPI();
-});
-
-
-
-</script>
+  </script>
 
 <body>
     <NavBar/>
