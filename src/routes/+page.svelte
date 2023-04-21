28ab15eb3305ad797e1c76577a5091dcbdadb563
Maukan Mir <maukanm22@gmail.com>
2023-04-21 13:49:31 -0400

    Added base url

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,8 +4,27 @@
     import { onMount } from "svelte";
     require('dotenv').config();
 
+    RAPID_NAME_KEY = process.env.RAPID_NAME_KEY
     RAPID_API_KEY = process.env.RAPID_API_KEY;
+
+    RAPID_NAME_HOST = process.env.RAPID_NAME_HOST
     RAPID_HOST = process.env.RAPID_HOST
+
+
+    const configureAPI = ()=>{
+        const options = {
+            method:"GET",
+            headers:{
+                RAPID_NAME_KEY: RAPID_API_KEY,
+		        RAPID_HOST: RAPID_HOST
+            }
+        }
+
+    const rapid_results = fetch('', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+    }
 
     let user;
     onMount(async () => {
