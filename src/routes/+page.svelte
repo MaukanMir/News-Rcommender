b6d13e6e4d4c7b9e2ea6af6c687b348f02e6ed59
Maukan Mir <maukanm22@gmail.com>
2023-04-22 19:46:48 -0400

    Added new styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,19 +10,31 @@
     const rapidNameHost = import.meta.env.VITE_RAPID_NAME_HOST;
     const rapidBaseUrl = import.meta.env.VITE_RAPID_BASE_URL;
 
-    const rapidResultsAPI = () => {
-    const options = {
-        method: "GET",
-        headers: {
-            "X-RapidAPI-Key": rapidApiKey,
-            "X-RapidAPI-Host": rapidHost,
-        },
-    };
-    fetch(rapidBaseUrl, options)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
-    };
+    const images = [
+        "https://images.pexels.com/photos/12328572/pexels-photo-12328572.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "https://images.pexels.com/photos/414943/pexels-photo-414943.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "https://images.pexels.com/photos/2267338/pexels-photo-2267338.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "https://images.pexels.com/photos/5794641/pexels-photo-5794641.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "https://images.pexels.com/photos/4799534/pexels-photo-4799534.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "https://images.pexels.com/photos/2258357/pexels-photo-2258357.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "https://images.pexels.com/photos/2538121/pexels-photo-2538121.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "https://images.pexels.com/photos/267350/pexels-photo-267350.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    ]
+
+    // const rapidResultsAPI = () => {
+    // const options = {
+    //     method: "GET",
+    //     headers: {
+    //         "X-RapidAPI-Key": rapidApiKey,
+    //         "X-RapidAPI-Host": rapidHost,
+    //     },
+    // };
+    // fetch(rapidBaseUrl, options)
+    //     .then(response => response.json())
+    //     .then(response => console.log(response))
+    //     .catch(err => console.error(err));
+    // };
+
     let user;
     onMount(async () => {
         userSession.subscribe(storeValue => {
@@ -37,9 +49,8 @@
 
 <body>
     <NavBar/>
-    <div class="image-container">
         <div class="container">
-        </div>
+            <img alt="" class="img" src ="https://images.pexels.com/photos/2258357/pexels-photo-2258357.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
     </div>
 
 </body>
@@ -52,9 +63,21 @@
         color:whitesmoke;
     }
 
-    .image-container{
-        display:flex;
+    .container{
+        display: flex;
         flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 1000px;
+        width: 2000px;
+    }
+
+    .img{
+    object-fit: cover;
+    border-radius: 10px;
+    height:1000px;
+    width:50%;
+    
     }
 
 </style>
