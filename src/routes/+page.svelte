f350e02db9648ce21f15c77472af7e5ac0273603
Maukan Mir <maukanm22@gmail.com>
2023-04-22 18:55:25 -0400

    Added api fetch

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,19 +8,20 @@
     const rapidHost = import.meta.env.VITE_RAPID_HOST;
     const rapidNameHost = import.meta.env.VITE_RAPID_NAME_HOST;
     const rapidBaseUrl = import.meta.env.VITE_RAPID_BASE_URL;
-    // const rapidResultsAPI = () => {
-    //   const options = {
-    //     method: "GET",
-    //     headers: {
-    //       [RAPID_NAME_KEY]: RAPID_API_KEY,
-    //       [RAPID_NAME_HOST]: RAPID_HOST,
-    //     },
-    //   };
-    //   fetch(RAPID_BASE_URL, options)
-    //     .then(response => response.json())
-    //     .then(response => console.log(response))
-    //     .catch(err => console.error(err));
-    // };
+
+    const rapidResultsAPI = () => {
+      const options = {
+        method: "GET",
+        headers: {
+          [rapidNameKey]: rapidApiKey,
+          [rapidNameHost]: rapidHost,
+        },
+      };
+      fetch(rapidBaseUrl, options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+    };
     let user;
     onMount(async () => {
         userSession.subscribe(storeValue => {
