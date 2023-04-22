05f278e84530856fc91d834838fc7a28e9022fa8
Maukan Mir <maukanm22@gmail.com>
2023-04-22 18:02:58 -0400

    Add new user information

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,43 +1,32 @@
 <!-- App.svelte -->
 <script>
-    import dotenv from 'dotenv';
-    dotenv.config();
-    const RAPID_NAME_KEY = process.env.RAPID_NAME_KEY;
-    const RAPID_API_KEY = process.env.RAPID_API_KEY;
-    const RAPID_NAME_HOST = process.env.RAPID_NAME_HOST;
-    const RAPID_HOST = process.env.RAPID_HOST;
-    const RAPID_BASE_URL = process.env.RAPID_BASE_URL;
-    console.log(RAPID_NAME_KEY, RAPID_API_KEY, RAPID_NAME_HOST, RAPID_HOST, RAPID_BASE_URL);
 
     import NavBar from "../lib/Nav.svelte";
     import { userSession } from "../stores/userSession";
     import { onMount } from "svelte";
-  
-    const rapidResultsAPI = () => {
-      const options = {
-        method: "GET",
-        headers: {
-          [RAPID_NAME_KEY]: RAPID_API_KEY,
-          [RAPID_NAME_HOST]: RAPID_HOST,
-        },
-      };
-  
-      fetch(RAPID_BASE_URL, options)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
-    };
-  
+    // const rapidResultsAPI = () => {
+    //   const options = {
+    //     method: "GET",
+    //     headers: {
+    //       [RAPID_NAME_KEY]: RAPID_API_KEY,
+    //       [RAPID_NAME_HOST]: RAPID_HOST,
+    //     },
+    //   };
+    //   fetch(RAPID_BASE_URL, options)
+    //     .then(response => response.json())
+    //     .then(response => console.log(response))
+    //     .catch(err => console.error(err));
+    // };
     let user;
     onMount(async () => {
-      userSession.subscribe(storeValue => {
+        userSession.subscribe(storeValue => {
         if (storeValue.user) {
-          user = storeValue.user;
+            user = storeValue.user;
         }
-      });
-      rapidResultsAPI();
     });
-  </script>
+    //   rapidResultsAPI();
+    });
+</script>
 
 <body>
     <NavBar/>
