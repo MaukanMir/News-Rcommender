756363df320613df09f4cfe5f363c201a0587f5a
Maukan Mir <maukanm22@gmail.com>
2023-04-23 19:44:04 -0400

    Added profile page

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -4,12 +4,16 @@ import {onMount} from "svelte";
 
 let user;
 let email;
+let dob;
+let _id;
 
 onMount(async()=>{
     userSession.subscribe(storeValue =>{
         if(storeValue.user){
             user = storeValue.user;
             email = storeValue.email;
+            dob = storeValue.dob;
+            _id = storeValue._id;
         }
     })
 })
