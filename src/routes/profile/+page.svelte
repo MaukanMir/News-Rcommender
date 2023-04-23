dc1d0c2140bd17145c6eccb829e900b6d9b8333d
Maukan Mir <maukanm22@gmail.com>
2023-04-23 19:42:07 -0400

    Added new styles

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -3,6 +3,15 @@ import {userSession} from "../../stores/userSession";
 import {onMount} from "svelte";
 
 let user;
+let email;
+
+onMount(async()=>{
+    userSession.subscribe(storeValue =>{
+        if(storeValue.user){
+            user = storeValue.user;
+        }
+    })
+})
 
 
 </script>
